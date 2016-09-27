eb70712591a5a9ac67479691d51ea5b357928e5f
Nell Shamrell <nellshamrell@gmail.com>
2016-09-27 14:29:40 -0700

    removing defunct fieri server setup code

--- fieri-server/output.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip" {
-  value = "${aws_instance.fieri-server.public_ip}"
-}
-
-output "public_dns" {
-  value = "${aws_instance.fieri-server.public_dns}"
-}
--- fieri-server/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {}
-variable "secret_key" {}
-variable "region" {
-  default = "us-west-2"
-}
-variable "instance_type" {}
-variable "ami" {}
-variable "security_groups" {}
-variable "key_name" {}
